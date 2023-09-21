7ea4f1929585c591db8e876730815cf037fb1b82
Aryan Shirbhate <121824389+Aryan7865@users.noreply.github.com>
2023-09-21 12:49:56 +0530

    Create Heroes.js

---
+++ Heroes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import HeroList from './HeroList';
+import useFetch from './useFetch';
+
+const Heroes = () => {
+  const { data: heroes, isPending, error } = useFetch('http://localhost:8000/heroes');
+
+  return (
+    <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {heroes && <HeroList heroes={heroes} />}
+    </div>
+  );
+};
+
+export default Heroes;
